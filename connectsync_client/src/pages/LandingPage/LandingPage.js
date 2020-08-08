1261946ba5ed37161e382c9b030aabe3333e3e09
Mohammed Abdul Jawad Vajeeh <51875089+jawadsab@users.noreply.github.com>
2020-08-08 20:05:12 +0530

    reac-redux added

--- connectsync_client/src/pages/LandingPage/LandingPage.js
+++ connectsync_client/src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./LandingPageStyles.scss";
 import { HeroSection } from "../../components/Landing/HeroSection";
 import { Navbar } from "../../components/Navigation/Navbar";
 
+
 export const LandingPage = () => {
+  //declare model ref
+  const modalRef = useRef();
+
+
   return (
     <div className="landing-page">
       <Navbar currentPage="LandingPage" />

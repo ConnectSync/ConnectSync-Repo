9a5cc01948cf37ae942f52804b3735028a529c4e
unknown <sachinbhatt669@gmail.com>
2020-08-09 02:02:31 +0530

    cors settings added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const config = require("config");
-const bodyParser = require("body-parser");
-const router = require("./router");
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const bodyParser = require('body-parser');
+const router = require('./router');
 
 // db config
-const db = config.get("mongoURI");
+const db = config.get('mongoURI');
 
 //connect to mongodb
 const connectDB = async () => {
@@ -17,7 +17,7 @@ const connectDB = async () => {
       useFindAndModify: false,
     });
 
-    console.log("MongoDB connected...");
+    console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
     process.exit(1); //exit process
@@ -31,10 +31,24 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+let allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,POST,PUT, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, x-auth-token'
+  );
+  next();
+};
+
+app.use(allowCrossDomain);
+
 app.use(router);
 
 // port
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });

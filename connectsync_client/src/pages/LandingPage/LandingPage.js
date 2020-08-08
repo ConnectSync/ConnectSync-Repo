bbcd84d36be21d396ca249617958bfe0c9d03527
Mohammed Abdul Jawad Vajeeh <51875089+jawadsab@users.noreply.github.com>
2020-08-08 20:51:44 +0530

    file setup

--- connectsync_client/src/pages/LandingPage/LandingPage.js
+++ connectsync_client/src/pages/LandingPage/LandingPage.js
@@ -2,11 +2,12 @@ import React, { useRef, useState } from "react";
 import "./LandingPageStyles.scss";
 import { HeroSection } from "../../components/Landing/HeroSection";
 import { Navbar } from "../../components/Navigation/Navbar";
+import AuthPopup from '../../components/Popup/AuthPopup';
+import { connect } from 'react-redux';
 
 
-export const LandingPage = () => {
-  //declare model ref
-  const modalRef = useRef();
+
+export const LandingPage = (props) => {
 
 
   return (
@@ -16,3 +17,9 @@ export const LandingPage = () => {
     </div>
   );
 };
+
+const mapStateToProps = (state) => ({
+  popup: state.popup,
+});
+
+export default connect(mapStateToProps)(Landing);

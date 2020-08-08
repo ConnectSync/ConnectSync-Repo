86678f778db00795ca35dcd9bbf2d00002dda9e3
Dhanmoni Nath <dhanmoni@users.noreply.github.com>
2020-08-08 21:26:07 +0530

    register and loaduser action added

--- connectsync_client/src/Routes.js
+++ connectsync_client/src/Routes.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { LandingPage } from "./pages/LandingPage/LandingPage";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
+import { LandingPage } from "./pages/LandingPage/LandingPage";
+
+import { loadUser } from "./redux/action/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 //all routing paths goes here
 const Routes = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
   return (
     <Provider store={store}>
       <Router>

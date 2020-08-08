9abb20fd346233ac8161e55e8e0cdee7e02ce1cc
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-08 10:55:47 +0800

    navbar

---
+++ connectsync_client/src/components/Landing/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [navStatus, setNavStatus] = useState("collapse navbar-collapse");
+  const navbarHandler = () => {
+    setNavbarOpen(!navbarOpen);
+    setNavStatus(
+      `collapse navbar-collapse text-center ${navbarOpen ? "" : "show"}`
+    );
+  };
+  return (
+    <div>
+      <nav className="navbar navbar-expand-sm navbar-light fixed-top">
+        <div className="container">
+          <div className="navbar-brand text-white">
+            <h3 className="m-0">ConnectSync</h3>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={navbarHandler}
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className={navStatus}>
+            {/* Conditional Render start */}
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item px-1">
+                <button className="btn btn-primary">Register</button>
+              </li>
+              <li className="nav-item px-1">
+                <button className="btn btn-secondary">Login</button>
+              </li>
+            </ul>
+            {/* Conditional Render end */}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
--- connectsync_client/src/pages/LandingPage/LandingPage.js
+++ connectsync_client/src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./LandingPageStyles.scss";
 import { HeroSection } from "../../components/Landing/HeroSection";
+import { Navbar } from "../../components/Landing/Navbar";
 
 export const LandingPage = () => {
   return (
     <div className="landing-page">
+      <Navbar />
       <HeroSection />
     </div>
   );

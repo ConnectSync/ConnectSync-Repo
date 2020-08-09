7f1c4da9c1a2dd477ef233c8b4a84f8ddcbc91fc
Dhanmoni Nath <dhanmoni@users.noreply.github.com>
2020-08-10 00:35:05 +0530

    workplace popup issue solved

--- connectsync_client/src/components/Profile/Profile.js
+++ connectsync_client/src/components/Profile/Profile.js
@@ -1,25 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { loadUser } from "../../redux/action/auth";
 import { getAllPosts } from "../../redux/action/posts";
 import { setActiveWorkplaces } from "../../redux/action/workplaces";
-import WorkPlacePopup from "../Popup/WorkplacePopup";
+import WorkplacePopup from "../Popup/WorkplacePopup";
+import { openChangePopup } from "../../redux/action/popup";
 
 import "./ProfileStyles.scss";
 
 const Profile = ({
   auth,
-  modalView,
   handleChange,
   create,
   join,
   workplace_name,
   workplace_description,
   workplace_type,
-  modelRef,
-  popupOpenModal,
   setActiveWorkplaces,
   getAllPosts,
+  popup,
+  openChangePopup,
+  loadUser,
   workplaces: { active_workplaces },
 }) => {
   const userProps = { ...auth.user };
@@ -84,78 +85,70 @@ const Profile = ({
     }
   });
 
-    
-    return(
-        <div className="profile all-center flex-column">
-            <div className="profileImageDiv mt-5 mb-4">
-                <img src={img} className="profileImg" />
-            </div>
-            <h5>{name}</h5>
-            <small>{email}</small>
-            <p className="lead border-top w-100 text-center m-0 py-3">
-            Active Workplaces{' '}
-            {!isEditing && (
-            <i className="fa fa-pencil" onClick={() => setIsEditing(true)}></i>
-            )}
-            </p>
-            {isEditing && (
-                <small className="mb-1">Select your active workplaces:</small>
-            )}
-            <ul className="w-100 p-0">
-                {isEditing ? editingWorkplaceList : activeWorkplaceList}
-            </ul>
-            {activeWorkplacesState.length < 1 && (
-                <small className="alert alert-danger">
-                Please select alteast one workplace!
-                </small>
-            )}
-            {isEditing === true && activeWorkplacesState.length > 0 ? (
-                <button
-                class="btn btn-secondary my-2 p-1 px-3"
-                onClick={() => {
-                    setIsEditing(false);
-                    setActiveWorkplaces(activeWorkplacesState);
-                    getAllPosts(active_workplaces);
-                }}
-                >
-                <small>Save</small>
-                </button>
-            ) : null}
-            {!isEditing && (
-            <div className="d-flex flex-column">
-                <button
-                    onClick={() => popupOpenModal('create')}
-                    className="px-5 btn-style mt-2 btn btn-primary shadow p-1 mb-2 bg-white rounded"
-                >
-                    Create +
-                </button>
-                <button
-                    onClick={() => popupOpenModal('join')}
-                    className="px-5 mt-2 btn-style btn btn-primary shadow p-1 mb-5 bg-white rounded"
-                >
-                    Join +
-                </button>
-            </div>
-
+  return (
+    <div className="profile all-center flex-column">
+      <div className="profileImageDiv mt-5 mb-4">
+        <img src={img} className="profileImg" />
+      </div>
+      <h5>{name}</h5>
+      <small>{email}</small>
+      <p className="lead border-top w-100 text-center m-0 py-3">
+        Active Workplaces{" "}
+        {!isEditing && (
+          <i className="fa fa-pencil" onClick={() => setIsEditing(true)}></i>
+        )}
+      </p>
+      {isEditing && (
+        <small className="mb-1">Select your active workplaces:</small>
       )}
-      <WorkPlacePopup
-        style={{
-          position: "absolute",
-          zIndex: 100,
-          top: 0,
-          left: 0,
-          right: 0,
-          height: 100 + "%",
-        }}
-        modalView={modalView}
-        handleChange={handleChange}
-        create={create}
-        join={join}
-        name={workplace_name}
-        description={workplace_description}
-        type={workplace_type}
-        modelRef={modelRef}
-      />
+      <ul className="w-100 p-0">
+        {isEditing ? editingWorkplaceList : activeWorkplaceList}
+      </ul>
+      {activeWorkplacesState.length < 1 && (
+        <small className="alert alert-danger">
+          Please select alteast one workplace!
+        </small>
+      )}
+      {isEditing === true && activeWorkplacesState.length > 0 ? (
+        <button
+          class="btn btn-secondary my-2 p-1 px-3"
+          onClick={() => {
+            setIsEditing(false);
+            setActiveWorkplaces(activeWorkplacesState);
+            getAllPosts(active_workplaces);
+          }}
+        >
+          <small>Save</small>
+        </button>
+      ) : null}
+      {!isEditing && (
+        <div className="d-flex flex-column">
+          <button
+            onClick={() => openChangePopup("JOIN")}
+            className="px-5 btn-style mt-2 btn btn-primary shadow p-1 mb-2 bg-white rounded"
+          >
+            Create +
+          </button>
+          <button
+            onClick={() => openChangePopup("JOIN")}
+            className="px-5 mt-2 btn-style btn btn-primary shadow p-1 mb-5 bg-white rounded"
+          >
+            Join +
+          </button>
+        </div>
+      )}
+
+      {(popup.activePopup === "CREATE" || popup.activePopup === "JOIN") && (
+        <WorkplacePopup
+          type={popup.activePopup}
+          handleChange={handleChange}
+          create={create}
+          join={join}
+          name={workplace_name}
+          description={workplace_description}
+          workplace_type={workplace_type}
+        />
+      )}
     </div>
   );
 };
@@ -163,10 +156,12 @@ const Profile = ({
 const mapStateToProps = (state) => ({
   auth: state.auth,
   workplaces: state.workplaces,
+  popup: state.popup,
 });
 
 export default connect(mapStateToProps, {
   loadUser,
   getAllPosts,
   setActiveWorkplaces,
+  openChangePopup,
 })(Profile);

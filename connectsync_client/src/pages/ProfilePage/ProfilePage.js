27bb8eb71116dce130e8a463988169203f62934e
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-09 16:19:29 +0800

    added edit button

--- connectsync_client/src/pages/ProfilePage/ProfilePage.js
+++ connectsync_client/src/pages/ProfilePage/ProfilePage.js
@@ -12,8 +12,9 @@ export const ProfilePage = () => {
         </div>
         <div className="all-center flex-column w-50 p-5 bg-white">
           <button class="btn btn-light btn-sm">Edit profile image</button>
-          <h5 className="pt-2">Name</h5>
+          <h5 className="mt-5">Name</h5>
           <small>Email</small>
+          <button class="btn btn-secondary btn-sm mt-3">Edit</button>
         </div>
 
         {/* workplaces */}

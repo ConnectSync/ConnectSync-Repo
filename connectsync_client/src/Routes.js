20c2c4215d7ca2abfca25e2324366fa65d0ed524
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-09 14:33:53 +0800

    merge from master

--- connectsync_client/src/Routes.js
+++ connectsync_client/src/Routes.js
@@ -1,22 +1,15 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { HomePage } from "./pages/HomePage/HomePage";
-import { LandingPage } from "./pages/LandingPage/LandingPage";
-=======
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/HomePage/HomePage';
-import LandingPage from './pages/LandingPage';
->>>>>>> 13277c04eea1b5a3b180943ba9d5f9c4f56e6d02
+import HomePage from "./pages/HomePage/HomePage";
+import LandingPage from "./pages/LandingPage";
 
-import { loadUser } from './redux/action/auth';
-import setAuthToken from './utils/setAuthToken';
+import { loadUser } from "./redux/action/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 // Redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);

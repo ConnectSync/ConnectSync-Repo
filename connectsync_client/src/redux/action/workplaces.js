427ee59ffe6bc69d42b27b85b4a8fb2059e19ca8
Dhanmoni Nath <dhanmoni@users.noreply.github.com>
2020-08-09 11:08:22 +0530

    join workplace added

--- connectsync_client/src/redux/action/workplaces.js
+++ connectsync_client/src/redux/action/workplaces.js
@@ -24,3 +24,29 @@ export const createWorkplace = (workplaceData) => async (dispatch) => {
     }
   }
 };
+
+export const joinWorkplace = (name) => async (dispatch) => {
+  try {
+    console.log(name);
+    const workplaceId = await api.get(`/workplace/public/${name}`);
+    console.log("id==", workplaceId);
+
+    const res = await api.post(`/workplace/${workplaceId.data._id}/join`);
+    console.log("workplace=", res.data);
+    dispatch({
+      type: JOIN_WORKPLACE,
+    });
+    dispatch(loadUser());
+    //dispatch(removeError());
+  } catch (err) {
+    dispatch(removeError());
+    console.log(err);
+    if (err.response) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setError(error.msg, "danger")));
+      }
+    }
+  }
+};
